9dcc4d342e361ea1334d6c85390e0d19e75affb2
Taytaywils <wilsjtay@gmail.com>
2025-12-14 19:41:25 -0700

    Update pictures.js

--- js/pictures.js
+++ js/pictures.js
@@ -38,29 +38,5 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-// document.addEventListener("DOMContentLoaded", async () => {
-//     try {
-//         const response = await fetch('/images.json');
-//         const data = await response.json();
 
-//         const beforeContainer = document.getElementById('insert-befores');
-//         const afterContainer  = document.getElementById('insert-afters');
-//         const otherContainer  = document.getElementById('insert-others');
-
-//         data.befores.forEach(src =>
-//             beforeContainer.appendChild(createImage(src, 'before-img'))
-//         );
-
-//         data.afters.forEach(src =>
-//             afterContainer.appendChild(createImage(src, 'after-img'))
-//         );
-
-//         data.others.forEach(src =>
-//             otherContainer.appendChild(createImage(src, 'others'))
-//         );
-
-//     } catch (err) {
-//         console.error("Failed to load images.json", err);
-//     }
-// });
 
